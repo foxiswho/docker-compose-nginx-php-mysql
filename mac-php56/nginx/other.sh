c6795d4e26e29b289846ca03569658ba10007716
fox <80005368@qq.com>
2016-11-13 15:21:43 +0800

    update

--- mac-php56/nginx/other.sh
+++ mac-php56/nginx/other.sh
@@ -2,7 +2,7 @@
 
 #用于手动操作
 
-echo "alias ls='ls -lhG --color=auto'" >> /root/.bashrc \
-&& echo "alias ll='ls -lhG --color=auto'" >> /root/.bashrc \
-&& source /root/.bashrc \
-&& sed '1i\fastcgi_param  SCRIPT_FILENAME    \$document_root\$fastcgi_script_name;' /etc/nginx/fastcgi_params > /etc/nginx/fastcgi
+#sed '1i\fastcgi_param  SCRIPT_FILENAME    \$document_root\$fastcgi_script_name;' /etc/nginx/fastcgi_params > /etc/nginx/fastcgi
+
+sed 's:include /etc/nginx/conf:#include /etc/nginx/conf:g;' /etc/nginx/include /etc/nginx/conf \
+&& sed "s:conf;:conf;\ninclude /Volumes/work/vhost/\*.conf;" /etc/nginx/include /etc/nginx/conf
