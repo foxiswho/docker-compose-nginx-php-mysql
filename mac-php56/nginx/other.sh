b987bf3aa43965a5d5b59097d0aa55840fa0ba2d
fox <80005368@qq.com>
2016-11-13 16:08:50 +0800

    配置

--- mac-php56/nginx/other.sh
+++ mac-php56/nginx/other.sh
@@ -5,4 +5,4 @@
 #sed '1i\fastcgi_param  SCRIPT_FILENAME    \$document_root\$fastcgi_script_name;' /etc/nginx/fastcgi_params > /etc/nginx/fastcgi
 
 sed -i 's:include /etc/nginx/conf:#include /etc/nginx/conf:g;' /etc/nginx/nginx.conf \
-&& sed -i "s:conf;:conf;\ninclude /Volumes/work/vhost/\*.conf;" /etc/nginx/nginx.conf
+&& sed -i "s:conf;:conf;\n    include /Volumes/work/vhost/\*.conf;:g" /etc/nginx/nginx.conf
